b4eb0e091c624200c6a2926ae02041a6bc1a15d8
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-09-25 11:20:39 -0500

    use the selected color I'm using elsewhere

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const char font[]        = "monospace:size=9";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
+static const char* selbgcolor   = "#005577";
 static const char* selfgcolor   = "#ffffff";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
